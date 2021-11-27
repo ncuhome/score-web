9a4b32220d1bd964a07076e5d99ae6f0f1515060
FuzzyFade <1339184537@qq.com>
2021-11-27 19:07:37 +0800

    refactor: use react-spring to boost Background animation

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import styles from './index.module.scss';
+import { animated, useSpring } from 'react-spring';
 
 const Background = () => {
+  const blueStyle = useSpring({
+    from: { x: -20, y: '70vh' },
+    to: { x: 10, y: '68vh' },
+    loop: { reverse: true },
+    config: { duration: 4000, frequency: 1 },
+  });
+
+  const redStyle = useSpring({
+    from: { y: '10vh' },
+    to: { y: '15vh' },
+    loop: { reverse: true },
+    config: { duration: 4000, frequency: 1 },
+  });
+
   return (
     <div className={styles.background}>
-      {/* {
-        Array(30).fill(1).map((item, index) => {
-          return <div key={index} className={styles.particle} />;
-        })
-      } */}
-      <div className={styles.left} />
-      <div className={styles.right} />
+      <animated.div className={styles.left} style={blueStyle} />
+      <animated.div className={styles.right} style={redStyle} />
     </div>
   );
 };
